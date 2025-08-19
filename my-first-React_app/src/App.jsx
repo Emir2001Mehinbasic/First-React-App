e542b4fda7df50716c405624c78351db64b1a083
Emir Mehinbasic <emir96meh@gmail.com>
2025-08-19 22:22:58 +0200

    Added Loading spinner for waiting to fetch data and removing message Loading...

--- my-first-React_app/src/App.jsx
+++ my-first-React_app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useEffect, useState } from 'react';
 import Search from './components/search';
+import Spinner from './components/Spinner';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -18,7 +19,7 @@ const App = () => {
    const [searchTerm , setSearchTerm] = useState('');
    const [errorMessage , setErrorMessage] = useState('');
    const [movieList, setMovieList] = useState([]);
-   const [isLoading, setIsLoading] = useState(true);
+   const [isLoading, setIsLoading] = useState(false);
 
 
    const fetchMovies = async ()=>{
@@ -45,7 +46,7 @@ const App = () => {
     console.error('Error fetching movies:', error);
     setErrorMessage('Error fetching movies')
    }finally {
-    setIsLoading(true);
+    setIsLoading(false);
    }
 }
    
@@ -64,22 +65,22 @@ const App = () => {
             Find <span className="text-gradient">Movies</span> You Enjoy
           </h1>
         </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <section className="all-movies">
           <h2>ALL MOVIES</h2>
           {isLoading ? (
-            <p className="text-white">Loading...</p>
-          ) : errorMessage ? (
+              <Spinner />
+             ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p key={movie.id} className='text-white'>{movie.title}</p>
+                <li key={movie.id} className='text-white'>{movie.title}</li>
               ))}
             </ul>
           )}
         </section>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
